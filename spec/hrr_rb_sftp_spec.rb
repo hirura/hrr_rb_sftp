b6e290d3d7ee85f4ba007d3c174409e8f56dc357
hirura <hirura@gmail.com>
2020-05-28 17:33:38 +0900

    update spec to remove unused case

--- spec/hrr_rb_sftp_spec.rb
+++ spec/hrr_rb_sftp_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe HrrRbSftp do
   it "has a version number" do
     expect(HrrRbSftp::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
