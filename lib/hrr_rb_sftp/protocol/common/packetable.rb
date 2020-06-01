0f25b83c38bba2b7253fad0d85c6662102abc817
hirura <hirura@gmail.com>
2020-06-01 19:52:11 +0900

    update protocol/common/packetable to support conditional format

--- lib/hrr_rb_sftp/protocol/common/packetable.rb
+++ lib/hrr_rb_sftp/protocol/common/packetable.rb
@@ -10,7 +10,7 @@ module HrrRbSftp
 
         def encode packet
           log_debug { 'encoding packet: ' + packet.inspect }
-          format = common_format
+          format = common_format + conditional_format(packet)
           format.map{ |data_type, field_name|
             begin
               field_value = packet[field_name]
@@ -25,14 +25,7 @@ module HrrRbSftp
         def decode payload
           payload_io = StringIO.new payload
           format = common_format
-          decoded_packet = format.map{ |data_type, field_name|
-            begin
-              [field_name, data_type.decode(payload_io)]
-            rescue => e
-              log_debug { "'field_name': #{field_name.inspect}" }
-              raise e
-            end
-          }.inject(Hash.new){ |h, (k, v)| h.update({k => v}) }
+          decoded_packet = decode_recursively(payload_io).inject(Hash.new){ |h, (k, v)| h.update({k => v}) }
           log_debug { 'decoded packet: ' + decoded_packet.inspect }
           decoded_packet
         end
@@ -41,6 +34,35 @@ module HrrRbSftp
 
         def common_format
           self.class::FORMAT
+        end
+
+        def conditional_format packet
+          return [] unless self.class.const_defined? :CONDITIONAL_FORMAT
+          packet.inject([]){ |a, (field_name, field_value)|
+            a + (self.class::CONDITIONAL_FORMAT.fetch(field_name, {})[field_value] || [])
+          }
+        end
+
+        def decode_recursively payload_io, packet=nil
+          if packet.class == Array and packet.size == 0
+            []
+          else
+            format = case packet
+                     when nil
+                       common_format
+                     when Array
+                       conditional_format(packet)
+                     end
+            decoded_packet = format.map{ |data_type, field_name|
+              begin
+                [field_name, data_type.decode(payload_io)]
+              rescue => e
+                log_debug { "'field_name': #{field_name.inspect}" }
+                raise e
+              end
+            }
+            decoded_packet + decode_recursively(payload_io, decoded_packet)
+          end
         end
       end
     end
--- spec/hrr_rb_sftp/protocol/common/packetable_spec.rb
+++ spec/hrr_rb_sftp/protocol/common/packetable_spec.rb
@@ -1,43 +1,155 @@
 RSpec.describe HrrRbSftp::Protocol::Common::Packetable do
-  let(:mixed_in){
-    Class.new do |klass|
-      include HrrRbSftp::Protocol::Common::Packetable
-      klass::TYPE = 255
-      klass::FORMAT = [
-        [HrrRbSftp::Protocol::Common::DataType::Byte,   :"type"],
-        [HrrRbSftp::Protocol::Common::DataType::Uint32, :"request-id"],
-        [HrrRbSftp::Protocol::Common::DataType::String, :"data"      ],
-      ]
-    end
-  }
+  context "when mixed-in class does not have CONDITIONAL_FORMAT" do
+    let(:mixed_in){
+      Class.new do |klass|
+        include HrrRbSftp::Protocol::Common::Packetable
+        klass::TYPE = 255
+        klass::FORMAT = [
+          [HrrRbSftp::Protocol::Common::DataType::Byte,   :"type"],
+          [HrrRbSftp::Protocol::Common::DataType::Uint32, :"request-id"],
+          [HrrRbSftp::Protocol::Common::DataType::String, :"data"      ],
+        ]
+      end
+    }
 
-  describe ".new" do
-    it "does not take arguments" do
-      expect{ mixed_in.new }.not_to raise_error
-    end
-  end
-
-  describe "#encode" do
-    packet = {:"type" => 255, :"request-id" => 123, :"data" => "testing"}
-
-    context "when each arg is acceptable by data_type" do
-      it "encodes #{packet.inspect} to \"FF 00 00 00 7B 00 00 00 07 t e s t i n g\"" do
-        expect( mixed_in.new.encode( packet ) ).to eq( ["FF", "0000007B", "00000007", "testing"].pack("H*H*H*a*") )
+    describe ".new" do
+      it "does not take arguments" do
+        expect{ mixed_in.new }.not_to raise_error
       end
     end
 
-    context "when an arg is not acceptable by data_type" do
-      it "raises an error" do
-        expect { mixed_in.new.encode( {:"type" => 255, :"request-id" => 123, :"data" => nil} ) }.to raise_error ArgumentError
+    describe "#encode" do
+      packet = {:"type" => 255, :"request-id" => 123, :"data" => "testing"}
+
+      context "when each arg is acceptable by data_type" do
+        it "encodes #{packet.inspect} to \"FF 00 00 00 7B 00 00 00 07 t e s t i n g\"" do
+          expect( mixed_in.new.encode( packet ) ).to eq( ["FF", "0000007B", "00000007", "testing"].pack("H*H*H*a*") )
+        end
+      end
+
+      context "when an arg is not acceptable by data_type" do
+        it "raises an error" do
+          expect { mixed_in.new.encode( {:"type" => 255, :"request-id" => 123, :"data" => nil} ) }.to raise_error ArgumentError
+        end
+      end
+    end
+
+    describe "#decode" do
+      packet = {:"type" => 255, :"request-id" => 123, :"data" => "testing"}
+
+      it "decodes \"FF 00 00 00 7B 00 00 00 07 t e s t i n g\" to #{packet.inspect}" do
+        expect( mixed_in.new.decode( ["FF", "0000007B", "00000007", "testing"].pack("H*H*H*a*") ) ).to eq packet
       end
     end
   end
 
-  describe "#decode" do
-    packet = {:"type" => 255, :"request-id" => 123, :"data" => "testing"}
+  context "when mixed-in class has CONDITIONAL_FORMAT" do
+    let(:mixed_in){
+      Class.new do |klass|
+        include HrrRbSftp::Protocol::Common::Packetable
+        klass::TYPE = 255
+        klass::FORMAT = [
+          [HrrRbSftp::Protocol::Common::DataType::Byte,   :"type"],
+          [HrrRbSftp::Protocol::Common::DataType::Uint32, :"request-id"],
+          [HrrRbSftp::Protocol::Common::DataType::String, :"data"      ],
+        ]
+        klass::TESTING_FORMAT = [
+          [HrrRbSftp::Protocol::Common::DataType::String, :"testing data"],
+        ]
+        klass::CONDITIONAL_FORMAT = {
+          :"data" => {
+            "testing" => klass::TESTING_FORMAT,
+          },
+        }
+      end
+    }
 
-    it "decodes \"FF 00 00 00 7B 00 00 00 07 t e s t i n g\" to #{packet.inspect}" do
-      expect( mixed_in.new.decode( ["FF", "0000007B", "00000007", "testing"].pack("H*H*H*a*") ) ).to eq packet
+    describe ".new" do
+      it "does not take arguments" do
+        expect{ mixed_in.new }.not_to raise_error
+      end
+    end
+
+    describe "#encode" do
+      packet = {:"type" => 255, :"request-id" => 123, :"data" => "testing", :"testing data" => "conditional"}
+
+      context "when each arg is acceptable by data_type" do
+        it "encodes #{packet.inspect} to \"FF 00 00 00 7B 00 00 00 07 t e s t i n g 00 00 00 0B c o n d i t i o n a l\"" do
+          expect( mixed_in.new.encode( packet ) ).to eq( ["FF", "0000007B", "00000007", "testing", "0000000B", "conditional"].pack("H*H*H*a*H*a*") )
+        end
+      end
+
+      context "when an arg is not acceptable by data_type" do
+        it "raises an error" do
+          expect { mixed_in.new.encode( {:"type" => 255, :"request-id" => 123, :"data" => "testing", :"testing data" => nil} ) }.to raise_error ArgumentError
+        end
+      end
+    end
+
+    describe "#decode" do
+      packet = {:"type" => 255, :"request-id" => 123, :"data" => "testing", :"testing data" => "conditional"}
+
+      it "decodes \"FF 00 00 00 7B 00 00 00 07 t e s t i n g 00 00 00 0B c o n d i t i o n a l\" to #{packet.inspect}" do
+        expect( mixed_in.new.decode( ["FF", "0000007B", "00000007", "testing", "0000000B", "conditional"].pack("H*H*H*a*H*a*") ) ).to eq packet
+      end
+    end
+  end
+
+  context "when mixed-in class has chained CONDITIONAL_FORMAT" do
+    let(:mixed_in){
+      Class.new do |klass|
+        include HrrRbSftp::Protocol::Common::Packetable
+        klass::TYPE = 255
+        klass::FORMAT = [
+          [HrrRbSftp::Protocol::Common::DataType::Byte,   :"type"],
+          [HrrRbSftp::Protocol::Common::DataType::Uint32, :"request-id"],
+          [HrrRbSftp::Protocol::Common::DataType::String, :"data"      ],
+        ]
+        klass::TESTING_FORMAT = [
+          [HrrRbSftp::Protocol::Common::DataType::String, :"testing data"],
+        ]
+        klass::CHAINED_FORMAT = [
+          [HrrRbSftp::Protocol::Common::DataType::String, :"chained data"],
+        ]
+        klass::CONDITIONAL_FORMAT = {
+          :"data" => {
+            "testing" => klass::TESTING_FORMAT,
+          },
+          :"testing data" => {
+            "conditional" => klass::CHAINED_FORMAT,
+          },
+        }
+      end
+    }
+
+    describe ".new" do
+      it "does not take arguments" do
+        expect{ mixed_in.new }.not_to raise_error
+      end
+    end
+
+    describe "#encode" do
+      packet = {:"type" => 255, :"request-id" => 123, :"data" => "testing", :"testing data" => "conditional", :"chained data" => "chained"}
+
+      context "when each arg is acceptable by data_type" do
+        it "encodes #{packet.inspect} to \"FF 00 00 00 7B 00 00 00 07 t e s t i n g 00 00 00 0B c o n d i t i o n a l 00 00 00 07 c h a i n e d\"" do
+          expect( mixed_in.new.encode( packet ) ).to eq( ["FF", "0000007B", "00000007", "testing", "0000000B", "conditional", "00000007", "chained"].pack("H*H*H*a*H*a*H*a*") )
+        end
+      end
+
+      context "when an arg is not acceptable by data_type" do
+        it "raises an error" do
+          expect { mixed_in.new.encode( {:"type" => 255, :"request-id" => 123, :"data" => "testing", :"testing data" => "conditional", :"chained data" => nil} ) }.to raise_error ArgumentError
+        end
+      end
+    end
+
+    describe "#decode" do
+      packet = {:"type" => 255, :"request-id" => 123, :"data" => "testing", :"testing data" => "conditional", :"chained data" => "chained"}
+
+      it "decodes \"FF 00 00 00 7B 00 00 00 07 t e s t i n g 00 00 00 0B c o n d i t i o n a l 00 00 00 07 c h a i n e d\" to #{packet.inspect}" do
+        expect( mixed_in.new.decode( ["FF", "0000007B", "00000007", "testing", "0000000B", "conditional", "00000007", "chained"].pack("H*H*H*a*H*a*H*a*") ) ).to eq packet
+      end
     end
   end
 end
