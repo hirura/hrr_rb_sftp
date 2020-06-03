fa0b1193945c2191217e842af344709cefb75c80
hirura <hirura@gmail.com>
2020-06-03 20:00:32 +0900

    implement version negotiation in server

--- lib/hrr_rb_sftp/server.rb
+++ lib/hrr_rb_sftp/server.rb
@@ -13,6 +13,36 @@ module HrrRbSftp
 
       @receiver = Receiver.new(@io_in)
       @sender   = Sender.new(@io_out)
+
+      negotiate_version
+      respond_to_requests
+    end
+
+    private
+
+    def negotiate_version
+      remote_version = receive_fxp_init
+      @version = [remote_version, Protocol.versions.keys.max].min
+      send_fxp_version
+    end
+
+    def receive_fxp_init
+      payload = @receiver.receive
+      packet = Protocol::Common::Packet::SSH_FXP_INIT.new(logger: logger).decode payload
+      packet[:"version"]
+    end
+
+    def send_fxp_version
+      packet = {
+        :"type"       => Protocol::Common::Packet::SSH_FXP_VERSION::TYPE,
+        :"version"    => @version,
+        :"extensions" => [],
+      }
+      payload = Protocol::Common::Packet::SSH_FXP_VERSION.new(logger: logger).encode packet
+      @sender.send payload
+    end
+
+    def respond_to_requests
     end
   end
 end
--- spec/hrr_rb_sftp/server_spec.rb
+++ spec/hrr_rb_sftp/server_spec.rb
@@ -18,18 +18,20 @@ RSpec.describe HrrRbSftp::Server do
     io.remote.err.close rescue nil
   end
 
-  context ".new" do
+  describe ".new" do
     it "takes no argument" do
       expect{ described_class.new }.not_to raise_error
     end
   end
 
-  context "#start" do
+  describe "#start" do
     let(:server){
       described_class.new
     }
 
-    it "takes in, out, err arguments" do
+    it "takes in, out, err arguments and starts negotiating version then responds to requests" do
+      expect( server ).to receive(:negotiate_version).with(no_args).once
+      expect( server ).to receive(:respond_to_requests).with(no_args).once
       expect{ server.start *io.local.to_a }.not_to raise_error
     end
 
@@ -39,6 +41,45 @@ RSpec.describe HrrRbSftp::Server do
 
     it "raises an error when more than 3 arguments" do
       expect{ server.start "arg1", "arg2", "arg3", "arg4" }.to raise_error ArgumentError
+    end
+  end
+
+  describe "#negotiate_version" do
+    let(:init_packet){
+      {
+        :"type"    => HrrRbSftp::Protocol::Common::Packet::SSH_FXP_INIT::TYPE,
+        :"version" => version,
+      }
+    }
+    let(:init_payload){
+      HrrRbSftp::Protocol::Common::Packet::SSH_FXP_INIT.new.encode(init_packet)
+    }
+
+    before :example do
+      @thread = Thread.new{
+        server = described_class.new
+        server.start *io.local.to_a
+      }
+    end
+
+    after :example do
+      @thread.kill
+    end
+
+    [1, 2].each do |version|
+      context "when remote protocol version is #{version}" do
+        let(:version){ version }
+
+        it "receives init with version #{version} and returns version with version #{version}" do
+          io.remote.in.write ([init_payload.length].pack("N") + init_payload)
+          payload_length = io.remote.out.read(4).unpack("N")[0]
+          payload = io.remote.out.read(payload_length)
+          expect( payload[0].unpack("C")[0] ).to eq HrrRbSftp::Protocol::Common::Packet::SSH_FXP_VERSION::TYPE
+          packet = HrrRbSftp::Protocol::Common::Packet::SSH_FXP_VERSION.new.decode(payload)
+          expect( packet[:"version"]    ).to eq version
+          expect( packet[:"extensions"] ).to eq []
+        end
+      end
     end
   end
 end
