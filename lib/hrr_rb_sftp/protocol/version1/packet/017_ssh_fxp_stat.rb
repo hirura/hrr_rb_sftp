a27bb26d8d48bf46eb95f5197d9f173a452c7090
hirura <hirura@gmail.com>
2020-06-04 18:54:55 +0900

    implement stat request response

--- lib/hrr_rb_sftp/protocol/version1/packet/017_ssh_fxp_stat.rb
+++ lib/hrr_rb_sftp/protocol/version1/packet/017_ssh_fxp_stat.rb
@@ -12,6 +12,42 @@ module HrrRbSftp
             [DataType::Uint32, :"request-id"],
             [DataType::String, :"path"      ],
           ]
+
+          def respond_to request
+            begin
+              stat = File.stat(request[:"path"])
+              attrs = Hash.new
+              attrs[:"size"]        = stat.size       if stat.size
+              attrs[:"uid"]         = stat.uid        if stat.uid
+              attrs[:"gid"]         = stat.gid        if stat.gid
+              attrs[:"permissions"] = stat.mode       if stat.mode
+              attrs[:"atime"]       = stat.atime.to_i if stat.atime
+              attrs[:"mtime"]       = stat.mtime.to_i if stat.mtime
+              {
+                :"type"       => SSH_FXP_ATTRS::TYPE,
+                :"request-id" => request[:"request-id"],
+                :"attrs"      => attrs,
+              }
+            rescue Errno::ENOENT
+              {
+                :"type"       => SSH_FXP_STATUS::TYPE,
+                :"request-id" => request[:"request-id"],
+                :"code"       => SSH_FXP_STATUS::SSH_FX_NO_SUCH_FILE,
+              }
+            rescue Errno::EACCES
+              {
+                :"type"       => SSH_FXP_STATUS::TYPE,
+                :"request-id" => request[:"request-id"],
+                :"code"       => SSH_FXP_STATUS::SSH_FX_PERMISSION_DENIED,
+              }
+            rescue
+              {
+                :"type"       => SSH_FXP_STATUS::TYPE,
+                :"request-id" => request[:"request-id"],
+                :"code"       => SSH_FXP_STATUS::SSH_FX_FAILURE,
+              }
+            end
+          end
         end
       end
     end
