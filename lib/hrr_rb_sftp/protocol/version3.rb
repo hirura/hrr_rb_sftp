f045e61e9813ba8a7f2540dd38abf3082146e717
hirura <hirura@gmail.com>
2020-06-04 20:44:49 +0900

    add protocol/version3/data_type module

--- lib/hrr_rb_sftp/protocol/version3.rb
+++ lib/hrr_rb_sftp/protocol/version3.rb
@@ -6,4 +6,5 @@ module HrrRbSftp
   end
 end
 
+require "hrr_rb_sftp/protocol/version3/data_type"
 require "hrr_rb_sftp/protocol/version3/packet"
---
+++ lib/hrr_rb_sftp/protocol/version3/data_type.rb
@@ -0,0 +1,9 @@
+module HrrRbSftp
+  class Protocol
+    class Version3
+      module DataType
+        include Version2::DataType
+      end
+    end
+  end
+end
