9c3f73e510be574739310a837951ac577b49430e
hirura <hirura@gmail.com>
2020-06-05 23:53:56 +0900

    update protocol/version1/packet/004_ssh_fxp_close request response to log error when specified handle does not exist

--- lib/hrr_rb_sftp/protocol/version1/packet/004_ssh_fxp_close.rb
+++ lib/hrr_rb_sftp/protocol/version1/packet/004_ssh_fxp_close.rb
@@ -13,30 +13,19 @@ module HrrRbSftp
             [DataType::String, :"handle"    ],
           ]
 
-          def close_and_delete_handle handle
-            @handles[handle].close rescue nil
-            @handles.delete(handle)
-          end
-
           def respond_to request
             begin
-              if close_and_delete_handle(request[:"handle"])
-                {
-                  :"type"          => SSH_FXP_STATUS::TYPE,
-                  :"request-id"    => request[:"request-id"],
-                  :"code"          => SSH_FXP_STATUS::SSH_FX_OK,
-                  :"error message" => "Success",
-                  :"language tag"  => "",
-                }
-              else
-                {
-                  :"type"          => SSH_FXP_STATUS::TYPE,
-                  :"request-id"    => request[:"request-id"],
-                  :"code"          => SSH_FXP_STATUS::SSH_FX_FAILURE,
-                  :"error message" => "Specified handle does not exist",
-                  :"language tag"  => "",
-                }
-              end
+              raise "Specified handle does not exist" unless @handles.has_key?(request[:"handle"])
+              handle = request[:"handle"]
+              @handles[handle].close rescue nil
+              @handles.delete(handle)
+              {
+                :"type"          => SSH_FXP_STATUS::TYPE,
+                :"request-id"    => request[:"request-id"],
+                :"code"          => SSH_FXP_STATUS::SSH_FX_OK,
+                :"error message" => "Success",
+                :"language tag"  => "",
+              }
             rescue => e
               log_error { [e.backtrace[0], ": ", e.message, " (", e.class.to_s, ")\n\t", e.backtrace[1..-1].join("\n\t")].join }
               {
