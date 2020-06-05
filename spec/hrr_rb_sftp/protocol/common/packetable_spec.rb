8651a0ee83c5ab4526d7982ee774b6ca9a10e2a0
hirura <hirura@gmail.com>
2020-06-05 20:19:24 +0900

    fix protocol/common/packetable spec with correct argument

--- spec/hrr_rb_sftp/protocol/common/packetable_spec.rb
+++ spec/hrr_rb_sftp/protocol/common/packetable_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe HrrRbSftp::Protocol::Common::Packetable do
 
     describe ".new" do
       it "does not take arguments" do
-        expect{ mixed_in.new }.not_to raise_error
+        expect{ mixed_in.new({}) }.not_to raise_error
       end
     end
 
@@ -23,13 +23,13 @@ RSpec.describe HrrRbSftp::Protocol::Common::Packetable do
 
       context "when each arg is acceptable by data_type" do
         it "encodes #{packet.inspect} to \"FF 00 00 00 7B 00 00 00 07 t e s t i n g\"" do
-          expect( mixed_in.new.encode( packet ) ).to eq( ["FF", "0000007B", "00000007", "testing"].pack("H*H*H*a*") )
+          expect( mixed_in.new({}).encode( packet ) ).to eq( ["FF", "0000007B", "00000007", "testing"].pack("H*H*H*a*") )
         end
       end
 
       context "when an arg is not acceptable by data_type" do
         it "raises an error" do
-          expect { mixed_in.new.encode( {:"type" => 255, :"request-id" => 123, :"data" => nil} ) }.to raise_error ArgumentError
+          expect { mixed_in.new({}).encode( {:"type" => 255, :"request-id" => 123, :"data" => nil} ) }.to raise_error ArgumentError
         end
       end
     end
@@ -38,7 +38,7 @@ RSpec.describe HrrRbSftp::Protocol::Common::Packetable do
       packet = {:"type" => 255, :"request-id" => 123, :"data" => "testing"}
 
       it "decodes \"FF 00 00 00 7B 00 00 00 07 t e s t i n g\" to #{packet.inspect}" do
-        expect( mixed_in.new.decode( ["FF", "0000007B", "00000007", "testing"].pack("H*H*H*a*") ) ).to eq packet
+        expect( mixed_in.new({}).decode( ["FF", "0000007B", "00000007", "testing"].pack("H*H*H*a*") ) ).to eq packet
       end
     end
   end
@@ -75,13 +75,13 @@ RSpec.describe HrrRbSftp::Protocol::Common::Packetable do
 
       context "when each arg is acceptable by data_type" do
         it "encodes #{packet.inspect} to \"FF 00 00 00 7B 00 00 00 07 t e s t i n g 00 00 00 0B c o n d i t i o n a l\"" do
-          expect( mixed_in.new.encode( packet ) ).to eq( ["FF", "0000007B", "00000007", "testing", "0000000B", "conditional"].pack("H*H*H*a*H*a*") )
+          expect( mixed_in.new({}).encode( packet ) ).to eq( ["FF", "0000007B", "00000007", "testing", "0000000B", "conditional"].pack("H*H*H*a*H*a*") )
         end
       end
 
       context "when an arg is not acceptable by data_type" do
         it "raises an error" do
-          expect { mixed_in.new.encode( {:"type" => 255, :"request-id" => 123, :"data" => "testing", :"testing data" => nil} ) }.to raise_error ArgumentError
+          expect { mixed_in.new({}).encode( {:"type" => 255, :"request-id" => 123, :"data" => "testing", :"testing data" => nil} ) }.to raise_error ArgumentError
         end
       end
     end
@@ -90,7 +90,7 @@ RSpec.describe HrrRbSftp::Protocol::Common::Packetable do
       packet = {:"type" => 255, :"request-id" => 123, :"data" => "testing", :"testing data" => "conditional"}
 
       it "decodes \"FF 00 00 00 7B 00 00 00 07 t e s t i n g 00 00 00 0B c o n d i t i o n a l\" to #{packet.inspect}" do
-        expect( mixed_in.new.decode( ["FF", "0000007B", "00000007", "testing", "0000000B", "conditional"].pack("H*H*H*a*H*a*") ) ).to eq packet
+        expect( mixed_in.new({}).decode( ["FF", "0000007B", "00000007", "testing", "0000000B", "conditional"].pack("H*H*H*a*H*a*") ) ).to eq packet
       end
     end
   end
@@ -133,13 +133,13 @@ RSpec.describe HrrRbSftp::Protocol::Common::Packetable do
 
       context "when each arg is acceptable by data_type" do
         it "encodes #{packet.inspect} to \"FF 00 00 00 7B 00 00 00 07 t e s t i n g 00 00 00 0B c o n d i t i o n a l 00 00 00 07 c h a i n e d\"" do
-          expect( mixed_in.new.encode( packet ) ).to eq( ["FF", "0000007B", "00000007", "testing", "0000000B", "conditional", "00000007", "chained"].pack("H*H*H*a*H*a*H*a*") )
+          expect( mixed_in.new({}).encode( packet ) ).to eq( ["FF", "0000007B", "00000007", "testing", "0000000B", "conditional", "00000007", "chained"].pack("H*H*H*a*H*a*H*a*") )
         end
       end
 
       context "when an arg is not acceptable by data_type" do
         it "raises an error" do
-          expect { mixed_in.new.encode( {:"type" => 255, :"request-id" => 123, :"data" => "testing", :"testing data" => "conditional", :"chained data" => nil} ) }.to raise_error ArgumentError
+          expect { mixed_in.new({}).encode( {:"type" => 255, :"request-id" => 123, :"data" => "testing", :"testing data" => "conditional", :"chained data" => nil} ) }.to raise_error ArgumentError
         end
       end
     end
@@ -148,7 +148,7 @@ RSpec.describe HrrRbSftp::Protocol::Common::Packetable do
       packet = {:"type" => 255, :"request-id" => 123, :"data" => "testing", :"testing data" => "conditional", :"chained data" => "chained"}
 
       it "decodes \"FF 00 00 00 7B 00 00 00 07 t e s t i n g 00 00 00 0B c o n d i t i o n a l 00 00 00 07 c h a i n e d\" to #{packet.inspect}" do
-        expect( mixed_in.new.decode( ["FF", "0000007B", "00000007", "testing", "0000000B", "conditional", "00000007", "chained"].pack("H*H*H*a*H*a*H*a*") ) ).to eq packet
+        expect( mixed_in.new({}).decode( ["FF", "0000007B", "00000007", "testing", "0000000B", "conditional", "00000007", "chained"].pack("H*H*H*a*H*a*H*a*") ) ).to eq packet
       end
     end
   end
