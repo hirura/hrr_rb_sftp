263754bfe4d7ea6fc37e159bc446991b73abdcb2
hirura <hirura@gmail.com>
2020-06-10 20:19:13 +0900

    add documentation for sender class

--- lib/hrr_rb_sftp/sender.rb
+++ lib/hrr_rb_sftp/sender.rb
@@ -1,9 +1,23 @@
 module HrrRbSftp
+
+  #
+  # This class implements payload sender.
+  #
   class Sender
+
+    #
+    # Instantiates a new payload sender for the output IO.
+    #
+    # @param io_out [IO] An IO for output.
+    #
     def initialize io_out
       @io_out = io_out
     end
 
+    #
+    # Sends payload_length then payload.
+    # Flushes output IO once payload is sent.
+    #
     def send payload
       @io_out.write(Protocol::Common::DataType::Uint32.encode(payload.bytesize))
       @io_out.write(payload)
