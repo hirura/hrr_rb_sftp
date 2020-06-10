a2c18c87be7f9dbd938a9d141621fc80dbfcea34
hirura <hirura@gmail.com>
2020-06-10 20:19:19 +0900

    add documentation for receiver class

--- lib/hrr_rb_sftp/receiver.rb
+++ lib/hrr_rb_sftp/receiver.rb
@@ -1,9 +1,24 @@
 module HrrRbSftp
+
+  #
+  # This class implements payload receiver.
+  #
   class Receiver
+
+    #
+    # Instantiates a new payload receiver for the input IO.
+    #
+    # @param io_in [IO] An IO for input.
+    #
     def initialize io_in
       @io_in = io_in
     end
 
+    #
+    # Receives payload_length payload. When input IO is EOF, returns nil.
+    #
+    # @return [String, nil] Received payload. when input IO is EOF, retruns nil.
+    #
     def receive
       begin
         paylaod_length = Protocol::Common::DataType::Uint32.decode(@io_in)
