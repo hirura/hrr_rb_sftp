80b5eda5eeee4a6bc77720f88f389de7d5293f0e
hirura <hirura@gmail.com>
2020-06-11 19:52:59 +0900

    add documentation for protocol/common/packet/001_ssh_fxp_init and 002_ssh_fxp_version classes

--- lib/hrr_rb_sftp/protocol/common/packet/002_ssh_fxp_version.rb
+++ lib/hrr_rb_sftp/protocol/common/packet/002_ssh_fxp_version.rb
@@ -2,11 +2,21 @@ module HrrRbSftp
   class Protocol
     module Common
       module Packet
+
+        #
+        # This class implements SFTP protocol version independent SSH_FXP_VERSION packet type, format, and responder.
+        #
         class SSH_FXP_VERSION
           include Packetable
 
+          #
+          # Represents SSH_FXP_VERSION packet type.
+          #
           TYPE = 2
 
+          #
+          # Represents SSH_FXP_VERSION packet format.
+          #
           FORMAT = [
             [DataType::Byte,           :"type"      ],
             [DataType::Uint32,         :"version"   ],
