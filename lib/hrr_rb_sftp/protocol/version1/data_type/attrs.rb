da5c04887962521b97ca7aa1e2af11f56707a7b2
hirura <hirura@gmail.com>
2020-06-11 20:07:59 +0900

    add documentation for protocol/version1/data_type/attrs module

--- lib/hrr_rb_sftp/protocol/version1/data_type/attrs.rb
+++ lib/hrr_rb_sftp/protocol/version1/data_type/attrs.rb
@@ -2,13 +2,44 @@ module HrrRbSftp
   class Protocol
     class Version1
       module DataType
+
+        #
+        # This module provides methods to convert file attributes represented in ::Hash and binary string each other.
+        #
         class Attrs
+
+          #
+          # Represents SSH_FILEXFER_ATTR_SIZE.
+          #
           SSH_FILEXFER_ATTR_SIZE        = 0x00000001
+
+          #
+          # Represents SSH_FILEXFER_ATTR_UIDGID.
+          #
           SSH_FILEXFER_ATTR_UIDGID      = 0x00000002
+
+          #
+          # Represents SSH_FILEXFER_ATTR_PERMISSIONS.
+          #
           SSH_FILEXFER_ATTR_PERMISSIONS = 0x00000004
+
+          #
+          # Represents SSH_FILEXFER_ATTR_ACMODTIME.
+          #
           SSH_FILEXFER_ATTR_ACMODTIME   = 0x00000008
+
+          #
+          # Represents SSH_FILEXFER_ATTR_EXTENDED.
+          #
           SSH_FILEXFER_ATTR_EXTENDED    = 0x80000000
 
+          #
+          # Converts file attributes represented in ::Hash into binary string.
+          #
+          # @param arg [::Hash{::Symbol=>::Object}] File attributes represented in ::Hash to be converted.
+          # @raise [::ArgumentError] When arg is not ::Hash value.
+          # @return [::String] Converted binary string.
+          #
           def self.encode arg
             unless arg.kind_of? ::Hash
               raise ArgumentError, "must be a kind of Hash, but got #{arg.inspect}"
@@ -33,6 +64,12 @@ module HrrRbSftp
             payload
           end
 
+          #
+          # Converts binary string into file attributes represented in ::Hash.
+          #
+          # @param io [::IO] ::IO instance that has buffer to be read.
+          # @return [::Hash{::Symbol=>::Object}] Converted file attributes represented in ::Hash.
+          #
           def self.decode io
             attrs = Hash.new
             flags                 = DataType::Uint32.decode(io)
