b3bf76cbaf26bc34783817460b521972ee639161
hirura <hirura@gmail.com>
2020-06-12 18:53:44 +0900

    add documentation for protocol/version2/data_type module

--- lib/hrr_rb_sftp/protocol/version2/data_type.rb
+++ lib/hrr_rb_sftp/protocol/version2/data_type.rb
@@ -1,6 +1,10 @@
 module HrrRbSftp
   class Protocol
     class Version2
+
+      #
+      # This module implements SFTP protocol version 2 data types to be used to encode or decode packet.
+      #
       module DataType
         include Version1::DataType
       end
