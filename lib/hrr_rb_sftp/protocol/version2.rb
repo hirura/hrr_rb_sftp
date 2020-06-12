aae100b9063781e8c76ffed46b3fc1f9569a6b03
hirura <hirura@gmail.com>
2020-06-12 18:53:08 +0900

    add documentation for protocol/version2 class

--- lib/hrr_rb_sftp/protocol/version2.rb
+++ lib/hrr_rb_sftp/protocol/version2.rb
@@ -1,6 +1,14 @@
 module HrrRbSftp
   class Protocol
+
+    #
+    # This class implements SFTP protocol version 2 packet types, formats, and responders.
+    #
     class Version2
+
+      #
+      # Represents SFTP protocol version 2.
+      #
       PROTOCOL_VERSION = 2
     end
   end
