ec479c04afb1632d782f3806087180ff7f639011
hirura <hirura@gmail.com>
2020-06-12 18:45:35 +0900

    add documentation for protocol/version1/packet/003_ssh_fxp_open class and make some constants and methods private in it

--- lib/hrr_rb_sftp/protocol/version1/packet/003_ssh_fxp_open.rb
+++ lib/hrr_rb_sftp/protocol/version1/packet/003_ssh_fxp_open.rb
@@ -2,11 +2,21 @@ module HrrRbSftp
   class Protocol
     class Version1
       module Packet
+
+        #
+        # This class implements SFTP protocol version 1 SSH_FXP_OPEN packet type, format, and responder.
+        #
         class SSH_FXP_OPEN
           include Common::Packetable
 
+          #
+          # Represents SSH_FXP_OPEN packet type.
+          #
           TYPE = 3
 
+          #
+          # Represents SSH_FXP_OPEN packet format.
+          #
           FORMAT = [
             [DataType::Byte,   :"type"      ],
             [DataType::Uint32, :"request-id"],
@@ -15,43 +25,45 @@ module HrrRbSftp
             [DataType::Attrs,  :"attrs"     ],
           ]
 
+          #
+          # Represents SSH_FXF_READ flag.
+          #
           SSH_FXF_READ   = 0x00000001
+
+          #
+          # Represents SSH_FXF_WRITE flag.
+          #
           SSH_FXF_WRITE  = 0x00000002
+
+          #
+          # Represents SSH_FXF_APPEND flag.
+          #
           SSH_FXF_APPEND = 0x00000004
+
+          #
+          # Represents SSH_FXF_CREAT flag.
+          #
           SSH_FXF_CREAT  = 0x00000008
+
+          #
+          # Represents SSH_FXF_TRUNC flag.
+          #
           SSH_FXF_TRUNC  = 0x00000010
+
+          #
+          # Represents SSH_FXF_EXCL flag.
+          #
           SSH_FXF_EXCL   = 0x00000020
 
-          class Error < StandardError
-          end
-
-          def convert_pflags_to_flags pflags
-            flags = 0
-            if ((pflags & SSH_FXF_READ) == SSH_FXF_READ) && ((pflags & SSH_FXF_WRITE) == SSH_FXF_WRITE)
-              flags |= ::File::RDWR
-            elsif (pflags & SSH_FXF_READ) == SSH_FXF_READ
-              flags |= ::File::RDONLY
-            elsif (pflags & SSH_FXF_WRITE) == SSH_FXF_WRITE
-              flags |= ::File::WRONLY
-            else
-              raise Error, "At least SSH_FXF_READ or SSH_FXF_READ must be specified"
-            end
-            if (pflags & SSH_FXF_APPEND) == SSH_FXF_APPEND
-              flags |= ::File::APPEND
-            end
-            if (pflags & SSH_FXF_CREAT) == SSH_FXF_CREAT
-              flags |= ::File::CREAT
-              flags |= ::File::TRUNC if (pflags & SSH_FXF_TRUNC) == SSH_FXF_TRUNC
-              flags |= ::File::EXCL  if (pflags & SSH_FXF_EXCL ) == SSH_FXF_EXCL
-            elsif (pflags & SSH_FXF_TRUNC) == SSH_FXF_TRUNC
-              raise Error, "SSH_FXF_CREAT MUST also be specified when SSH_FXF_TRUNC is specified"
-            elsif (pflags & SSH_FXF_EXCL) == SSH_FXF_EXCL
-              raise Error, "SSH_FXF_CREAT MUST also be specified when SSH_FXF_EXCL is specified"
-            end
-            flags |= ::File::BINARY
-            flags
-          end
-
+          #
+          # Responds to SSH_FXP_OPEN request.
+          #
+          # @param request [Hash{Symbol=>Object}] SSH_FXP_OPEN request represented in Hash.
+          #                                       Only permissions attribute is taken care of.
+          #                                       When attrs field contains attributes other than permissions are ignored
+          #                                       and they are expected to be taken care of by subsequent SSH_FXP_SETSTAT and/or SSH_FXP_FSETSTAT requests.
+          # @return [Hash{Symbol=>Object}] Response represented in Hash. In case of success, its type is SSH_FXP_HANDLE. In other cases, its type is SSH_FXP_STATUS.
+          #
           def respond_to request
             begin
               flags = convert_pflags_to_flags request[:"pflags"]
@@ -101,6 +113,40 @@ module HrrRbSftp
                 :"language tag"  => "",
               }
             end
+          end
+
+          class Error < StandardError
+          end
+
+          private_constant :Error
+
+          private
+
+          def convert_pflags_to_flags pflags
+            flags = 0
+            if ((pflags & SSH_FXF_READ) == SSH_FXF_READ) && ((pflags & SSH_FXF_WRITE) == SSH_FXF_WRITE)
+              flags |= ::File::RDWR
+            elsif (pflags & SSH_FXF_READ) == SSH_FXF_READ
+              flags |= ::File::RDONLY
+            elsif (pflags & SSH_FXF_WRITE) == SSH_FXF_WRITE
+              flags |= ::File::WRONLY
+            else
+              raise Error, "At least SSH_FXF_READ or SSH_FXF_READ must be specified"
+            end
+            if (pflags & SSH_FXF_APPEND) == SSH_FXF_APPEND
+              flags |= ::File::APPEND
+            end
+            if (pflags & SSH_FXF_CREAT) == SSH_FXF_CREAT
+              flags |= ::File::CREAT
+              flags |= ::File::TRUNC if (pflags & SSH_FXF_TRUNC) == SSH_FXF_TRUNC
+              flags |= ::File::EXCL  if (pflags & SSH_FXF_EXCL ) == SSH_FXF_EXCL
+            elsif (pflags & SSH_FXF_TRUNC) == SSH_FXF_TRUNC
+              raise Error, "SSH_FXF_CREAT MUST also be specified when SSH_FXF_TRUNC is specified"
+            elsif (pflags & SSH_FXF_EXCL) == SSH_FXF_EXCL
+              raise Error, "SSH_FXF_CREAT MUST also be specified when SSH_FXF_EXCL is specified"
+            end
+            flags |= ::File::BINARY
+            flags
           end
         end
       end
