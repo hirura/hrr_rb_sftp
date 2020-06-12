22928c7cffe6fcb191c7f360a0c275b0aa707042
hirura <hirura@gmail.com>
2020-06-12 18:54:56 +0900

    add documentation for protocol/version3 class

--- lib/hrr_rb_sftp/protocol/version3.rb
+++ lib/hrr_rb_sftp/protocol/version3.rb
@@ -1,6 +1,14 @@
 module HrrRbSftp
   class Protocol
+
+    #
+    # This class implements SFTP protocol version 3 packet types, formats, and responders.
+    #
     class Version3
+
+      #
+      # Represents SFTP protocol version 3.
+      #
       PROTOCOL_VERSION = 3
     end
   end
