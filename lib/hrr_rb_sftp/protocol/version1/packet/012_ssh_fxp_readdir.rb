356d9416c89bc1d14790391c5f4e2379a5fc8de3
hirura <hirura@gmail.com>
2020-06-12 18:48:06 +0900

    add documentation for protocol/version1/packet/012_ssh_fxp_readdir class and make some methods private in it

--- lib/hrr_rb_sftp/protocol/version1/packet/012_ssh_fxp_readdir.rb
+++ lib/hrr_rb_sftp/protocol/version1/packet/012_ssh_fxp_readdir.rb
@@ -2,16 +2,76 @@ module HrrRbSftp
   class Protocol
     class Version1
       module Packet
+
+        #
+        # This class implements SFTP protocol version 1 SSH_FXP_READDIR packet type, format, and responder.
+        #
         class SSH_FXP_READDIR
           include Common::Packetable
 
+          #
+          # Represents SSH_FXP_READDIR packet type.
+          #
           TYPE = 12
 
+          #
+          # Represents SSH_FXP_READDIR packet format.
+          #
           FORMAT = [
             [DataType::Byte,   :"type"      ],
             [DataType::Uint32, :"request-id"],
             [DataType::String, :"handle"    ],
           ]
+
+          #
+          # Responds to SSH_FXP_READDIR request.
+          #
+          # @param request [Hash{Symbol=>Object}] SSH_FXP_READDIR request represented in Hash.
+          # @return [Hash{Symbol=>Object}] Response represented in Hash. In case of success, its type is SSH_FXP_NAME. In other cases, its type is SSH_FXP_STATUS.
+          #
+          def respond_to request
+            begin
+              raise "Specified handle does not exist" unless @handles.has_key?(request[:"handle"])
+              dir = @handles[request[:"handle"]]
+              raise "Specified handle is not directory" unless dir.instance_of?(::Dir)
+              entries = ::Array.new
+              while entry = dir.read
+                entries.push entry
+              end
+              unless entries.empty?
+                response = {
+                  :"type"       => SSH_FXP_NAME::TYPE,
+                  :"request-id" => request[:"request-id"],
+                  :"count"      => entries.size,
+                }
+                entries.each.with_index do |entry, idx|
+                  response[:"filename[#{idx}]"] = entry
+                  response[:"longname[#{idx}]"] = longname(dir, entry)
+                  response[:"attrs[#{idx}]"]    = attrs(dir, entry)
+                end
+                response
+              else
+                {
+                  :"type"          => SSH_FXP_STATUS::TYPE,
+                  :"request-id"    => request[:"request-id"],
+                  :"code"          => SSH_FXP_STATUS::SSH_FX_EOF,
+                  :"error message" => "End of file",
+                  :"language tag"  => "",
+                }
+              end
+            rescue => e
+              log_error { [e.backtrace[0], ": ", e.message, " (", e.class.to_s, ")\n\t", e.backtrace[1..-1].join("\n\t")].join }
+              {
+                :"type"          => SSH_FXP_STATUS::TYPE,
+                :"request-id"    => request[:"request-id"],
+                :"code"          => SSH_FXP_STATUS::SSH_FX_FAILURE,
+                :"error message" => e.message,
+                :"language tag"  => "",
+              }
+            end
+          end
+
+          private
 
           def longname_permissions stat
             s = ::String.new
@@ -84,48 +144,6 @@ module HrrRbSftp
             attrs[:"atime"]       = stat.atime.to_i if stat.atime
             attrs[:"mtime"]       = stat.mtime.to_i if stat.mtime
             attrs
-          end
-
-          def respond_to request
-            begin
-              raise "Specified handle does not exist" unless @handles.has_key?(request[:"handle"])
-              dir = @handles[request[:"handle"]]
-              raise "Specified handle is not directory" unless dir.instance_of?(::Dir)
-              entries = ::Array.new
-              while entry = dir.read
-                entries.push entry
-              end
-              unless entries.empty?
-                response = {
-                  :"type"       => SSH_FXP_NAME::TYPE,
-                  :"request-id" => request[:"request-id"],
-                  :"count"      => entries.size,
-                }
-                entries.each.with_index do |entry, idx|
-                  response[:"filename[#{idx}]"] = entry
-                  response[:"longname[#{idx}]"] = longname(dir, entry)
-                  response[:"attrs[#{idx}]"]    = attrs(dir, entry)
-                end
-                response
-              else
-                {
-                  :"type"          => SSH_FXP_STATUS::TYPE,
-                  :"request-id"    => request[:"request-id"],
-                  :"code"          => SSH_FXP_STATUS::SSH_FX_EOF,
-                  :"error message" => "End of file",
-                  :"language tag"  => "",
-                }
-              end
-            rescue => e
-              log_error { [e.backtrace[0], ": ", e.message, " (", e.class.to_s, ")\n\t", e.backtrace[1..-1].join("\n\t")].join }
-              {
-                :"type"          => SSH_FXP_STATUS::TYPE,
-                :"request-id"    => request[:"request-id"],
-                :"code"          => SSH_FXP_STATUS::SSH_FX_FAILURE,
-                :"error message" => e.message,
-                :"language tag"  => "",
-              }
-            end
           end
         end
       end
